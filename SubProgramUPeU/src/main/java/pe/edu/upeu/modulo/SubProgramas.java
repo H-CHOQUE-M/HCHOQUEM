2f2b586992a72fb7acb44287aac44e3a2bb5a9df
H-CHOQUE-M <>
2021-11-21 01:32:08 +0000

    Subprogramas

---
+++ SubProgramUPeU/src/main/java/pe/edu/upeu/modulo/SubProgramas.java
@@ -0,0 +1,20 @@
+package pe.edu.upeu.modulo;
+import java.util.Scanner;
+
+public class SubProgramas{
+
+  public String nombre="Dario";
+  private String dni="";
+  public Scanner sc=new Scanner(System.in);
+
+  public void saludo(){
+    System.out.println("Hola"+nombre);
+  }
+  public void otrosaludo(){
+    String apellidos="Perez";
+
+    System.out.println("Hola"+nombre+""+apellidos);
+  }
+
+
+}
